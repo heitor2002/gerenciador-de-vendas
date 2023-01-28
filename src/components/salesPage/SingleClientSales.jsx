f05d3eb682bd81c751285eb61000af619bf8e737
heitor2002 <montagnini.heitor@gmail.com>
2023-01-28 20:26:58 -0300

    feat: adição de painel de vendas.

--- src/components/salesPage/SingleClientSales.jsx
+++ src/components/salesPage/SingleClientSales.jsx
@@ -11,20 +11,23 @@ const SingleClientSales = () => {
     "http://localhost:3000/requests"
   );
 
-  const [productPrice, setProductPrice] = useState(null)
-  const [productQuantity, setProductQuantity] = useState(1)
+  const [productPrice, setProductPrice] = useState(null);
+  const [productQuantity, setProductQuantity] = useState(1);
+  const [selectedProductName, setSelectedProductName] = useState("");
+  const [selectedProductPrice, setSelectedProductPrice] = useState(0);
+  const [selectedProductQuantity, setSelectedProductQuantity] = useState(0);
 
   const productStockList = fetchStock.map((info) => {
     let arrayProducts = info.productsList;
     return arrayProducts;
   });
 
-  var amount = productPrice * productQuantity
+  var amount = productPrice * productQuantity;
 
   const arrayStock = productStockList.flat();
 
-  console.log(fetchClient)
-  console.log(arrayStock)
+  // console.log(fetchClient)
+  // console.log(arrayStock)
   return (
     <>
       <div className="container">
@@ -34,26 +37,49 @@ const SingleClientSales = () => {
         <div className="flex-content-list-products-for-sale">
           <div className="list-products">
             <ul>
-              {arrayStock.map(product => {
-                return(
+              {arrayStock.map((product) => {
+                let productQuantity = product.productQuantity;
+                let productPrice = product.productPrice;
+                let productName = product.productName;
+                return (
                   <>
-                  <li>{product.productName}</li>
+                    <li
+                      onClick={() => {
+                        setSelectedProductName(productName);
+                        setSelectedProductPrice(productPrice);
+                        setSelectedProductQuantity(productQuantity);
+                      }}
+                    >
+                      {productName}
+                    </li>
                   </>
-                )
+                );
               })}
             </ul>
           </div>
           <div className="selected-product">
             <div className="card-selected-product">
-              <h2>Produto: </h2>
-              <h3>Quantidade disponível:</h3>
-              <h3>Comprado por: R$</h3>
+              <h2>Produto: {selectedProductName}</h2>
+              <h3>Quantidade disponível: {selectedProductQuantity}</h3>
+              <h3>Comprado por: R${selectedProductPrice}</h3>
               <form>
                 <label>Preço da venda: R$</label>
-                <input type="number" name="sale_price" id="sale_price" value={productPrice} onChange={(e) => setProductPrice(e.target.value)}/>
+                <input
+                  type="number"
+                  name="sale_price"
+                  id="sale_price"
+                  value={productPrice}
+                  onChange={(e) => setProductPrice(e.target.value)}
+                />
                 <br />
                 <label>Quantidade:</label>
-                <input type="number" name="sale_quantity" id="sale_quantity" value={productQuantity} onChange={(e) => setProductQuantity(e.target.value)}/>
+                <input
+                  type="number"
+                  name="sale_quantity"
+                  id="sale_quantity"
+                  value={productQuantity}
+                  onChange={(e) => setProductQuantity(e.target.value)}
+                />
                 <br />
                 <h3>Valor total da venda: R$ {amount.toFixed(2)}</h3>
                 <input type="submit" value="Vender" name="acao" />
