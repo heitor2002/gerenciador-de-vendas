3fa55962c7220a362011fe5d5e837cdd7b05c02a
heitor2002 <montagnini.heitor@gmail.com>
2023-01-27 21:11:45 -0300

    style: estilização da página sales.

--- src/components/salesPage/SingleClientSales.jsx
+++ src/components/salesPage/SingleClientSales.jsx
@@ -1,9 +1,46 @@
+import { useParams } from "react-router-dom";
+import fetchClients from "../../fetchClients";
+
 const SingleClientSales = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  const { id } = useParams();
+  const { dataFetchInformations: fetchClient } = fetchClients(
+    "http://localhost:3000/clients/" + id
+  );
+  const { dataFetchInformations: fetchStock } = fetchClients(
+    "http://localhost:3000/requests"
+  );
+
+  const productStockList = fetchStock.map((info) => {
+    let arrayProducts = info.productsList;
+    return arrayProducts;
+  });
+
+  const arrayStock = productStockList.flat();
+
+  console.log(fetchClient)
+  console.log(arrayStock)
+  return (
+    <>
+      <div className="container">
+        <div className="title-page">
+          <h2>{fetchClient.clientName}</h2>
+        </div>
+        <div className="flex-content-list-products-for-sale">
+          <div className="list-products">
+            <ul>
+              {arrayStock.map(product => {
+                return(
+                  <>
+                  <li>{product.productName}</li>
+                  </>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SingleClientSales;
