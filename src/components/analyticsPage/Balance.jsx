47ad3e59ca7a06b51230876c0fd8e858f7711b89
heitor2002 <montagnini.heitor@gmail.com>
2023-02-03 14:05:47 -0300

    feat: cálculo de desempenho.

--- src/components/analyticsPage/Balance.jsx
+++ src/components/analyticsPage/Balance.jsx
@@ -1,33 +1,47 @@
 import fetchClients from "../../fetchClients";
 
 const Balance = () => {
-    const ports = {
-        clients: 3000,
-        paymentHistory: 3500,
-        requests: 4000,
-        sales: 4500,
-        stock: 5000,
-      };
-    const {dataFetchInformations: sales} = fetchClients(`http://localhost:${ports.sales}/sales`)
-    const {dataFetchInformations: requests} = fetchClients(`http://localhost:${ports.requests}/requests`)
+  const ports = {
+    clients: 3000,
+    paymentHistory: 3500,
+    requests: 4000,
+    sales: 4500,
+    stock: 5000,
+  };
+  const { dataFetchInformations: sales } = fetchClients(
+    `http://localhost:${ports.sales}/sales`
+  );
+  const { dataFetchInformations: requests } = fetchClients(
+    `http://localhost:${ports.requests}/requests`
+  );
 
-    const mapAllSales = sales.map(item => {
-        return item.floatInputPrice * item.floatInputQuantity
+  //MAPEANDO E SOMANDO TODAS AS VENDAS
+  const allSalesValue = sales
+    .map((item) => {
+      return item.floatInputPrice * item.floatInputQuantity;
     })
-    const someAllSales = mapAllSales.reduce((acc, item) => {
-        return acc + item;
+    .reduce((acc, item) => {
+      return acc + item;
+    }, 0);
+
+  // MAPEANDO E SOMANDO TODOS OS PEDIDOS
+  const allRequestsValue = requests
+    .map((item) => {
+      return parseFloat(item.accumulatedRequestValue);
     })
+    .reduce((acc, item) => {
+      return acc + item;
+    }, 0);
 
-    const mapAllRequestsValue = requests.map(item => {
-        return parseFloat(item.accumulatedRequestValue)
-    })
-
-    console.log(someAllSales)
-    return (
-        <>
-
-        </>
-    )
-}
+  return (
+    <>
+      <div className="requests-sales-payments">
+        <h2>Soma total de pedidos: R$<span>{allRequestsValue.toFixed(2)}</span></h2>
+        <h2>Vendas realizadas: R$<span>{allSalesValue.toFixed(2)}</span></h2>
+        <h2>Pagamentos pendentes: R$<span></span></h2>
+      </div>
+    </>
+  );
+};
 
 export default Balance;
