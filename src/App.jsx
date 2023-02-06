4702d14e6229176de7a920e910663a4e1095f825
heitor2002 <montagnini.heitor@gmail.com>
2023-02-06 14:26:31 -0300

    style: adaptando menu para tamanho de tela.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <>
       <Router>
         <div className="flex-panel-content">
-          {/* <Header /> */}
+          <Header />
           <MobileMenu />
           <div className="panel-content">
             <Routes>
