6233f6b707ec6c51c5363382b8f9c8167060dd65
heitor2002 <montagnini.heitor@gmail.com>
2023-02-08 22:35:24 -0300

    refactor: retorno para componente original.

--- src/components/clientsPage/InformationClient.jsx
+++ src/components/clientsPage/InformationClient.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import { AiFillEdit } from "react-icons/ai";
+
 const InformationClient = (props) => {
+  const handleEditProfile = (e) => {
+    e.preventDefault();
+  };
+
+  const [clientName, setClientName] = useState(clientInformation.clientName);
+  // const [clientNickname, setClientNickname] = useState("");
+  // const [clientCity, setClientCity] = useState("");
+  // const [clientAddress, setClientAddress] = useState("");
+  // const [clientDistrict, setClientDistrict] = useState("");
+  // const [clientNumberAddress, setClientNumberAddress] = useState();
+  // const [clientTellNumber, setClientTellNumber] = useState();
   return (
     <>
       <h2>Informações:</h2>
@@ -18,6 +32,27 @@ const InformationClient = (props) => {
       <h3>
         Telefone: <span>{props.phone}</span>
       </h3>
+      <button>
+        Editar:
+        <AiFillEdit />
+      </button>
+      <div className="edit-profile">
+        <form onSubmit={handleEditProfile}>
+          <input
+            type="text"
+            value={clientName}
+            onChange={(e) => setClientName(e.target.value)}
+          />
+          {/* <input type="text" value={clientNickname} placeholder="Apelido" />
+          <input type="text" value={clientCity} />
+          <input type="text" value={clientAddress} />
+          <input type="number" value={clientNumberAddress} />
+          <input type="text" value={clientDistrict} />
+          <input type="number" value={clientTellNumber} /> */}
+          <input type="submit" value={"Editar"} name={"acao"} />
+          <button className="cancel-edit-profile">Cancelar</button>
+        </form>
+      </div>
     </>
   );
 };
